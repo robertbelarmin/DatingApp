3e65f55c672e549bb26e437164f063dd77f3b1b1
robertbelarmin <robertvetri@gmail.com>
2021-12-11 13:16:48 -0500

    registered form and  replaysubject async

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'client';
+  title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit() {
+    this.setCurrentUser();
   }
 
-  ngOnInit(): void {
-    this.getUsers();
-  }
-
-  private getUsers() {
-    this.http.get('https://localhost:5001/api/Users').subscribe(res => {
-      this.users = res;
-    }, error => {
-      console.log(error);
-    });
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
