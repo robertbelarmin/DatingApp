e9e414a6e811f80f5df87791d43b096b774b790b
robertbelarmin <robertvetri@gmail.com>
2021-12-26 22:17:24 -0500

    Automapper, seeding Age extension Repository patte

--- API/DTOs/PhotoDto.cs
+++ API/DTOs/PhotoDto.cs
@@ -1,10 +1,9 @@
-namespace API.Entities
+namespace API.DTOs
 {
-    public class Photo
+    public class PhotoDto
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
-        public string PublicId  { get; set; }
     }
 }
